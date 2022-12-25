a0f23bffd2604d056b39aa71cfd2eebedd027d30
miwanoff <marina.iva.noffa@gmail.com>
2022-12-25 15:53:56 +0200

    Add function out()

---
+++ action.php
@@ -0,0 +1,22 @@
+<?php
+include_once "dbconnect.php";
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+function out($count)
+{
+    global $conn;
+    $arr_out = [];
+    try {
+        if (!$result = $conn->query("SELECT * GBookTable ORDER BY date DESC LIMIT " . $count)) {
+            throw new Exception('Error selection from table  GBookTable: [' . $conn->error . ']');
+        }
+        while ($row = $result->fetch_assoc()) {
+            $arr_out[] = $row;
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+    return $arr_out;
+}
