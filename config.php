0a5000e9b33c850155d6d0a548c84ffcfd94b1ba
miwanoff <marina.iva.noffa@gmail.com>
2022-12-25 15:20:42 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+const SERVERNAME = "127.0.0.1";
+const DBNAME = "Guestbook22";
+const USERNAME = "root";
+const PASSWORD = "";
---
+++ create_DB.php
@@ -0,0 +1,21 @@
+<?php
+include_once "config.php";
+
+try {
+// Create a connection
+    $conn = new mysqli(SERVERNAME, USERNAME, PASSWORD);
+// Connection test
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+// Database creation
+    $sql = "CREATE DATABASE " . DBNAME . " CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci";
+    if (!$conn->query($sql)) {
+        throw new Exception('Error creating database: ['.$conn->error.']');
+    } else {
+        echo "Database created successfully";
+    }
+    $conn->close();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
