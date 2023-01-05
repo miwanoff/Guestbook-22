8bd398675565d5141e808a8f9ea0d3a9ff60c0d2
miwanoff <marina.iva.noffa@gmail.com>
2023-01-05 18:17:19 +0200

    Add authorization

--- action.php
+++ action.php
@@ -20,3 +20,36 @@ function out($count)
     }
     return $arr_out;
 }
+
+function check_autorize($log, $pas) {
+	global $conn; 
+	$sql = "SELECT log FROM Users WHERE log = '" . $log . "' AND pas='" . $pas . "';";
+	
+	if ($result = $conn->query($sql)) {
+		$n = $result->num_rows; 
+		if ($n != 0) {
+			$_SESSION ['user_login'] = $log; 
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+function logout() { 
+	unset($_SESSION ['user_login']);
+	session_unset();
+	header("Location: index.php");
+}
+
+if (isset($_REQUEST ['action'])) { 
+	$action = $_REQUEST ['action'];
+	switch ($action) {		
+		case 'logout' :
+			logout();
+			break; 
+		default :
+			header("Location: index.php"); 
+	}
+}
--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 include_once "action.php";
 
 include "header.php";
+
+$c=0;	
+if (isset($_SESSION ['user_login'])) {
+	echo "<a href='admin_panel.php'>Увійти до адміністративної панелі</a><br/>";
+	echo "<a href='action.php?action=logout'>Вийти з облікового запису</a><br/>";
+}	
+else {
+	echo "<a href='autorize.php'>Увійти</a><br/>";
+	echo "<a href='registration.php'>Зареєструватись</a><br/>";
+}
 $out = out(5);
 print_r($out);
 if (count($out) > 0) {
@@ -9,17 +19,17 @@ if (count($out) > 0) {
 
         ?>
 <div style="margin:10px; padding:5px;width:450px;background:f0f0f0;">
-    <div style="color: #999999; border-bottom:1px solid #999999;padding:5px;">Опубликовал: <span
+    <div style="color: #999999; border-bottom:1px solid #999999;padding:5px;">Опублікував: <span
             style="color: #444;font-weight: bold;"><?php echo $row['username']; ?></span></div>
     <div style="background:#fafafa;padding:5px;"><?php echo $row['message']; ?></div>
-    <div style="color: #999999; border-top:1px solid #999999;padding:5px;">Дата публикации: <?php echo $row['date']; ?>
+    <div style="color: #999999; border-top:1px solid #999999;padding:5px;">Дата публикації: <?php echo $row['date']; ?>
     </div>
 </div>
 <?php
 }
-} else // если ни одной записи не встретилось
+} else 
 {
-    echo "В гостевой книге пока нет записей...<br>";
+    echo "В гостьовій книжці поки що немає записів...<br>";
 }
 
 include "footer.php";
